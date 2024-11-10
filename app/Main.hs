2c460b54f2b4dd4191d8d9f7d216f5a81a923a63
Redon Xharja <redon.xharja@gmail.com>
2024-11-09 23:28:24 -0500

    moved out pure logic from stateful ones

--- app/Main.hs
+++ app/Main.hs
@@ -47,54 +47,80 @@ toInt Two = 2
 sumHands :: Hand -> Hand -> Parity
 sumHands p1Hand p2Hand = if even (toInt p1Hand + toInt p2Hand) then Even else Odd
 
+initialState :: Settings -> GameState
+initialState sets = GameState { settings = sets, p1Score = 0, p2Score = 0 }
+
+updateGameType :: Hand -> GameType -> GameType
+updateGameType p1Hand (CPU ng) = CPU (update p1Hand ng)
+updateGameType _ gt = gt
+
+winningHand :: Hand -> Parity -> Hand
+winningHand One Odd = One
+winningHand One Even = Two
+winningHand Two Odd = Two
+winningHand Two Even = One
+
+congratulate :: Bool -> String
+congratulate p1Win = if p1Win then "\nPlayer 1 wins!" else "\nPlayer 2 wins!"
+
+incScore :: GameState -> Bool -> GameState
+incScore gs p1Win = if p1Win 
+  then gs { p1Score = p1Score gs + 1 } 
+  else gs { p2Score = p2Score gs + 1 }
+
+showParity :: Parity -> String
+showParity p = "\nPlayer 1 is " ++ show p ++ " and player 2 is " ++ show (oppositeParity p)
+
+showScore :: Score -> Score -> String
+showScore s1 s2 = "\ncurrent scores: P1: " ++ show s1 ++ " P2:  " ++ show s2
+
 ---------- Set Up ---------- 
 askGameMode :: IO GameType
 askGameMode = do
   putStrLn "1. Computer Game\n2. Human"
   x <- getChar
-  case x of 
+  case x of
     '1' -> return (CPU $ ngram 2 3)
     '2' -> return TwoPlayer
     _ -> putStrLn "Choose 1 or 2." >> askGameMode
-  
+
 setupSettings :: IO Settings
 setupSettings = do
   gameMode <- askGameMode
   parity <- randomOfTwo Odd Even
-  putStrLn $ "\nPlayer 1 is " ++ show parity ++ " and player 2 is " ++ show (oppositeParity parity)
+  putStrLn (showParity parity)
   pure Settings { gameType = gameMode, playerParity = parity }
 
 setupGame :: IO GameState
-setupGame = do
-  settings' <- setupSettings
-  pure GameState { settings = settings', p1Score = 0, p2Score = 0 }
+setupGame = initialState <$> setupSettings
 
 ---------- Input ---------- 
+liftPrint :: String -> Game ()
+liftPrint = liftIO . putStrLn
+
 getHumanThrow :: IO Hand
-getHumanThrow = do 
+getHumanThrow = do
   x <- readMaybe <$> fmap pure getChar :: IO (Maybe Int)
-  case x of 
+  case x of
     Just 1 -> return One
     Just 2 -> return Two
     _ -> putStr "\nPlease choose either 1 or 2 fingers: " >> getHumanThrow
 
+getCpuThrow :: NGram Hand -> Parity -> Hand
+getCpuThrow ng = winningHand (predictNext ng) 
+
 getP2Throw :: Settings -> IO Hand
-getP2Throw Settings { gameType = TwoPlayer } = getHumanThrow 
-getP2Throw Settings { gameType = CPU ng, playerParity = parity } = pure $
-  case (predictNext ng, parity) of 
-   (One, Odd) -> One
-   (One, Even) -> Two
-   (Two, Odd) -> Two
-   (Two, Even) -> One
+getP2Throw Settings { gameType = TwoPlayer } = getHumanThrow
+getP2Throw Settings { gameType = CPU ng, playerParity = p } = pure $ getCpuThrow ng p
 
 ---------- Core ---------- 
 throwHands :: Settings -> IO (Hand, Parity)
 throwHands sets = do
   hSetEcho stdin False
   putStr "Player 1 - Enter either 1 or 2: "
-  p1Throw <- getHumanThrow 
+  p1Throw <- getHumanThrow
 
-  when (isTwoPlayer $ gameType sets) $ 
+  when (isTwoPlayer $ gameType sets) $
     putStr "\nPlayer 2 - Enter either 1 or 2: "
 
   p2Throw <- getP2Throw sets
@@ -103,33 +129,29 @@ throwHands sets = do
 
   return (p1Throw, sumHands p1Throw p2Throw)
 
-showScore :: Score -> Score -> IO ()
-showScore s1 s2 = putStrLn $ "\ncurrent scores: P1: " ++ show s1 ++ " P2:  " ++ show s2
-
 updateScore :: Game Hand
 updateScore = do
-  gs@GameState{ settings = sets, p1Score = s1, p2Score = s2 } <- get
+  gs@GameState{ settings = sets } <- get
   (p1Hand, parity) <- liftIO $ throwHands sets
-
-  let p1Win = playerParity sets == parity 
-  liftIO . putStrLn $ if p1Win then "\nPlayer 1 wins!" else "\nPlayer 2 wins!"
-  put $ if p1Win then gs { p1Score = s1 + 1 } else gs { p2Score = s2 + 1 }
-
+  let p1Win = playerParity sets == parity
+  liftPrint $ congratulate p1Win
+  put (incScore gs p1Win)
   pure p1Hand
-  
+
 updateNGram :: Hand -> Game ()
-updateNGram p1Hand = modify $ \gs -> case settings gs of  
-  Settings { gameType = CPU ng } ->
-    gs { settings = (settings gs) { gameType = CPU (update p1Hand ng) } }
-  _ -> gs
+updateNGram p1Hand = modify $ \gs@GameState { settings = sets } -> 
+  gs { settings = sets { gameType = updateGameType p1Hand (gameType sets) } }
 
 runGame :: Game ()
 runGame = do
   GameState { p1Score = s1, p2Score = s2 } <- get
-  liftIO $ showScore s1 s2
+  liftPrint $ showScore s1 s2
   updateScore >>= updateNGram >> runGame
 
+setBuffering :: IO ()
+setBuffering = do
+  hSetBuffering stdout NoBuffering
+  hSetBuffering stdin  NoBuffering
+
 main :: IO ()
-main = do hSetBuffering stdout NoBuffering 
-          hSetBuffering stdin  NoBuffering 
-          setupGame >>= evalStateT runGame 
+main = setBuffering >> setupGame >>= evalStateT runGame
